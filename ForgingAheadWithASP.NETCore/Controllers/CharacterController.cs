d7bd4491942b1ca719ab5648dcaf3aa446f7d130
iAvinashVarma <iavinashvarma@gmail.com>
2017-03-05 19:30:39 +0530

    Enhancement #1: Upload the project repository.

---
+++ ForgingAheadWithASP.NETCore/Controllers/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using ForgingAheadWithASP.NETCore.Models;
+
+namespace ForgingAheadWithASP.NETCore.Controllers
+{
+	public class CharacterController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public CharacterController(ApplicationDbContext context)
+		{
+			this._context = context;
+		}
+
+		public IActionResult Create(Character character)
+		{
+			_context.Characters.Add(character);
+			_context.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ ForgingAheadWithASP.NETCore/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForgingAheadWithASP.NETCore.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+		public DbSet<Character> Characters { get; set; }
+    }
+}
